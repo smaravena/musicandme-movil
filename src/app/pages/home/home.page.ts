eb8968efdf0386195401f1c82de6dfeeb5a20f77
Aravena <134702678+smaravena@users.noreply.github.com>
2024-08-30 20:08:57 -0400

    remove unusued import

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
-import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +15,6 @@ export class HomePage {
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    this.modal.dismiss(null, 'confirm');
   }
 
 }
