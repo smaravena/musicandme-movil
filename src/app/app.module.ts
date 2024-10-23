a2ae28d80216d9298d646ad149dfa5c68e6d7296
Aravena <134702678+smaravena@users.noreply.github.com>
2024-10-23 12:45:31 -0300

    [Add] BarcodeScanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, BarcodeScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/qr-usuario/qr-usuario.page.ts
+++ src/app/pages/qr-usuario/qr-usuario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-qr-usuario',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrUsuarioPage implements OnInit {
 
-  constructor() { }
+  code: any
+  constructor(private barcodeScanner: BarcodeScanner) { }
 
   ngOnInit() {
   }
 
+  scannerQr(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.code = barcodeData.text
+    console.log('Barcode data', barcodeData);
+    }).catch(err => {
+    console.log('Error', err);
+    })
+    }
 }
